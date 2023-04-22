4c5df4f2a7811c76d5ad69fc1e3360ff15ec7665
EsmerlinJM <esmerlinjm@gmail.com>
2023-04-21 22:04:13 -0300

    [FEAT]: Add source code.

---
+++ go.mod
@@ -0,0 +1,31 @@
+module webhook-ngrok-tunnel
+
+go 1.20
+
+require (
+	github.com/google/go-github/v51 v51.0.0
+	golang.ngrok.com/ngrok v1.0.0
+	golang.org/x/oauth2 v0.7.0
+)
+
+require (
+	github.com/ProtonMail/go-crypto v0.0.0-20230217124315-7d5c6f04bbb8 // indirect
+	github.com/cloudflare/circl v1.1.0 // indirect
+	github.com/go-stack/stack v1.8.1 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/inconshreveable/log15 v3.0.0-testing.3+incompatible // indirect
+	github.com/inconshreveable/log15/v3 v3.0.0-testing.5 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/jpillora/backoff v1.0.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	golang.org/x/crypto v0.7.0 // indirect
+	golang.org/x/net v0.9.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/term v0.7.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,192 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/google/go-github/v51/github"
+	"github.com/joho/godotenv"
+	"golang.ngrok.com/ngrok"
+	"golang.ngrok.com/ngrok/config"
+	"golang.org/x/oauth2"
+	"golang.org/x/sync/errgroup"
+)
+
+var (
+	githubToken = os.Getenv("GITHUB_TOKEN")
+	repo = os.Getenv("REPO")
+	owner = os.Getenv("OWNER")
+	webHookEndpoint = os.Getenv("WEBHOOK_ENDPOINT")
+)
+
+func main() {
+	if _, err := os.Stat(".env"); err == nil {
+		godotenv.Load(".env")
+		githubToken = os.Getenv("GITHUB_TOKEN")
+		repo = os.Getenv("REPO")
+		owner = os.Getenv("OWNER")
+		webHookEndpoint = os.Getenv("WEBHOOK_ENDPOINT")
+	}
+
+    if err := run(context.Background()); err != nil {
+        log.Fatal(err)
+    }
+}
+
+func run(ctx context.Context) error {
+    ngrokHooks, err := fetchNgrokHooks(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	if len(ngrokHooks) > 0 {
+		for _, hook := range ngrokHooks {
+			url := hook.Config["url"].(string)
+
+			if strings.Contains(url, "ngrok") {
+				deleteNgrokHook(ctx, hook.ID)
+				break
+			}
+		}
+	}
+
+	tunnel, err := makeNgrokTunnel(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	tunnelUrl := tunnel.URL()+webHookEndpoint
+
+	log.Println("tunnel created:", tunnelUrl)
+
+	config := map[string]interface{} {
+		"url": flag.String("url", tunnelUrl, "The URL to which the payloads will be delivered."),
+	}
+
+	webHook := &github.Hook{
+		Name:         flag.String("Name", "ngrok tunnel", "Name of webhook"),
+		Events:       []string{"push", "pull_request"},
+		Active:       flag.Bool("Active", true, "Tunnel is active"),
+		Config: config,
+	}
+
+	fmt.Println("Creating hook...")
+
+	time.Sleep(3 * time.Second)
+	
+	hookCreated := createNgrokHook(ctx, webHook)
+
+	log.Println("Hook created: ", *hookCreated.URL)
+
+	for {
+		conn, err := tunnel.Accept()
+		if err != nil {
+			return err
+		}
+
+		log.Println("accepted connection from", conn.RemoteAddr())
+
+		go func() {
+			des := os.Getenv("PRPIVATE_ADDRESS")
+			err := handleConnection(ctx, des, conn)
+			log.Println("connection closed:", err)
+		}()
+	}
+}
+
+func fetchNgrokHooks(ctx context.Context) ([]*github.Hook, error) {
+	githubToken := os.Getenv("GITHUB_TOKEN")
+	repo := os.Getenv("REPO")
+	owner := os.Getenv("OWNER")
+
+    tokenService := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: githubToken},
+	)
+    tc := oauth2.NewClient(ctx, tokenService)
+    client := github.NewClient(tc)
+
+    hooks, _, err := client.Repositories.ListHooks(ctx, owner, repo, nil)
+
+	if err != nil {
+		fmt.Printf("Problem in getting repository information %v\n", err)
+	}
+	
+    if _, ok := err.(*github.AbuseRateLimitError); ok {
+        log.Println("hit rate limit")
+    }
+
+
+	return hooks, err
+}
+
+func createNgrokHook(ctx context.Context, hook *github.Hook) (*github.Hook) {
+    tokenService := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: githubToken},
+	)
+    tc := oauth2.NewClient(ctx, tokenService)
+    client := github.NewClient(tc)
+
+    hook, _, err := client.Repositories.CreateHook(ctx, owner, repo, hook)
+
+	if err != nil {
+		fmt.Printf("Problem in getting repository information %v\n", err)
+		os.Exit(1)
+	}
+
+	return hook
+}
+
+func deleteNgrokHook(ctx context.Context, hookId *int64) {
+    tokenService := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: githubToken},
+	)
+    tc := oauth2.NewClient(ctx, tokenService)
+    client := github.NewClient(tc)
+
+    hook, err := client.Repositories.DeleteHook(ctx, owner, repo, *hookId)
+
+	if err != nil {
+		fmt.Printf("Problem in getting repository information %v\n", err)
+		os.Exit(1)
+	}
+
+	log.Println("Hook deleted:", hook.Status)
+}
+
+func makeNgrokTunnel(ctx context.Context) (ngrok.Tunnel, error) {
+	tun, err := ngrok.Listen(ctx,
+        config.HTTPEndpoint(),
+        ngrok.WithAuthtokenFromEnv(),
+    )
+
+    return tun, err
+}
+
+func handleConnection(ctx context.Context, dest string, conn net.Conn) error {
+	next, err := net.Dial("tcp", dest)
+	if err != nil {
+		return err
+	}
+
+	g, _ := errgroup.WithContext(ctx)
+
+	g.Go(func() error {
+		_, err := io.Copy(next, conn)
+		return err
+	})
+	g.Go(func() error {
+		_, err := io.Copy(conn, next)
+		return err
+	})
+
+	return g.Wait()
+}
